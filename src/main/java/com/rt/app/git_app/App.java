ff733959e123f185a3d838d9fcba8d281084be6c
Rta Diaz <rtadiaz552@gmail.com>
2022-02-08 19:58:37 +0530

    added git-app project to local repo

---
+++ src/main/java/com/rt/app/git_app/App.java
@@ -0,0 +1,18 @@
+package com.rt.app.git_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void addData(String data){
+		
+		System.out.println("data=" +data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
