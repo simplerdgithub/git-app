8550dce4cad2f6787e763b04d22ed0baf347988c
Rta Diaz <rtadiaz552@gmail.com>
2022-02-08 20:02:31 +0530

    added if condition to check the data

--- src/main/java/com/rt/app/git_app/App.java
+++ src/main/java/com/rt/app/git_app/App.java
@@ -10,6 +10,10 @@ public class App
 	public void addData(String data){
 		
 		System.out.println("data=" +data);
+	
+	if(data.equals("java techie")){
+		System.out.println("Welcome");
+	}
 	}
     public static void main( String[] args )
     {
